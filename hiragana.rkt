14744f75a927ba039f723db0b5a429408eb0268b
Kevin Robert Stravers <macocio@gmail.com>
2017-04-10 11:28:11 +0200

    Make the alphabet without making everything into a character

--- hiragana.rkt
+++ hiragana.rkt
@@ -8,17 +8,17 @@ Write exit or press control+c or control+d to exit.")
 
 (define alphabet
   '(
-    (#\あ "a") (#\い "i") (#\う "u") (#\え "e") (#\お "o")
-    (#\か "ka") (#\き "ki") (#\く "ku") (#\け "ke") (#\こ "ko")
-    (#\さ "sa") (#\し "shi") (#\す "su") (#\せ "se") (#\そ "so")
-    (#\た "ta") (#\ち "chi") (#\つ "tsu") (#\て "te") (#\と "to")
-    (#\な "na") (#\に "ni") (#\ぬ "nu") (#\ね "ne") (#\の "no")
-    (#\は "ha") (#\ひ "hi") (#\ふ "hu") (#\へ "he") (#\ほ "ho")
-    (#\ま "ma") (#\み "mi") (#\む "mu") (#\め "me") (#\も "mo")
-    (#\や "ya") (#\ゆ "yu") (#\よ "yo")
-    (#\ら "ra") (#\り "ri") (#\る "ru") (#\れ "re") (#\ろ "ro")
-    (#\わ "wa") #| (#\ゐ "wi") (#\ゑ "we") |# (#\を "wo")
-    (#\ん "n")
+    (あ "a") (い "i") (う "u") (え "e") (お "o")
+    (か "ka") (き "ki") (く "ku") (け "ke") (こ "ko")
+    (さ "sa") (し "shi") (す "su") (せ "se") (そ "so")
+    (た "ta") (ち "chi") (つ "tsu") (て "te") (と "to")
+    (な "na") (に "ni") (ぬ "nu") (ね "ne") (の "no")
+    (は "ha") (ひ "hi") (ふ "hu") (へ "he") (ほ "ho")
+    (ま "ma") (み "mi") (む "mu") (め "me") (も "mo")
+    (や "ya") (ゆ "yu") (よ "yo")
+    (ら "ra") (り "ri") (る "ru") (れ "re") (ろ "ro")
+    (わ "wa") #| (ゐ "wi") (ゑ "we") |# (を "wo")
+    (ん "n")
   ))
 
 (define (select-random-letter#io letter-count)
@@ -40,7 +40,7 @@ Write exit or press control+c or control+d to exit.")
 
 (let loop ([current (select-random-letter#io start-letter-count)]
            [letter-count start-letter-count])
-  (display (string-append (string (first current)) ": "))
+  (display (string-append (symbol->string (first current)) ": "))
   (let ([input (read-line)])
     (cond
       ([or (eof-object? input) (string=? input "exit")] void)
