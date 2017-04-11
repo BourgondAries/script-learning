b37811bef40abcd6e753faa3368802d5e57445f3
Kevin Robert Stravers <macocio@gmail.com>
2017-04-11 17:30:22 +0200

    Prevent the same letters after each other and display the least entered letters at a rate of 25%.

--- kana.rkt
+++ kana.rkt
@@ -21,13 +21,36 @@ Input  ka        correct answer for か or カ (similar for other kana).
 (define-simple-macro (defines (name:id value:expr) ...+)
   (begin (define name value) ...))
 
+(struct/lens entry (rights wrongs) #:prefab)
+
 (defines (start-letter-count 5)
          (hiragana first)
          (katakana second)
-         (romaji   third))
+         (romaji   third)
+         (rights entry-rights-lens)
+         (wrongs entry-wrongs-lens))
 
-(define (select-random-letter#io letter-count)
-  (list-ref alphabet (random letter-count)))
+(define (score-letter letter statistic)
+  (if (hash-has-key? statistic letter)
+    (let ([entry (hash-ref statistic letter)])
+      (- (entry-rights entry) (* 2 (entry-wrongs entry))))
+    0))
+
+(define (random-from-list list)
+  (list-ref list (random (length list))))
+
+(define (select-random-letter#io letter-count statistics kana previous)
+  (define percentage 25)
+  (define multiplier (/ 1 (/ percentage 100)))
+  (let ([prev-new (if (= letter-count 1) 'none previous)])
+    (~>>
+      (take alphabet letter-count)
+      (filter (lambda (x) (not (equal? (kana x) prev-new))))
+      (map (lambda (letter) (list letter (score-letter (kana letter) statistics))))
+      (sort _ (lambda (x y) (< (second x) (second y))))
+      ((lambda (list) (take list (ceiling (/ (length list) multiplier)))))
+      (map first)
+      random-from-list)))
 
 (define (is-digit? character)
   (<= 48 (char->integer character) 57))
@@ -47,11 +70,9 @@ Input  ka        correct answer for か or カ (similar for other kana).
     (display i))
   (newline))
 
-(struct/lens entry (rights wrongs) #:prefab)
 
-(define (add1-to-current hash current correct-or-wrong)
-  (let* ([c-or-w (if (symbol=? correct-or-wrong 'right) entry-rights-lens entry-wrongs-lens)]
-         [this-entry (lens-compose c-or-w (hash-ref-lens current))])
+(define (add1-to-statistics hash current which)
+  (let* ([this-entry (lens-compose which (hash-ref-lens current))])
     (if (hash-has-key? hash current)
       (lens-transform this-entry hash add1)
       (~>
@@ -73,7 +94,7 @@ Input  ka        correct answer for か or カ (similar for other kana).
       (sort _ (lambda (x y) (> (occurrence x) (occurrence y))) #:key second)
       (map (lambda (x) (list (first x) 'right: (entry-rights (second x)) 'wrong: (entry-wrongs (second x))))))))
 
-(let loop ([current (select-random-letter#io start-letter-count)]
+(let loop ([current (select-random-letter#io start-letter-count (make-immutable-hash) hiragana 'none)]
            [letter-count start-letter-count]
            [kana hiragana]
            [statistics (make-immutable-hash)])
@@ -91,18 +112,18 @@ Input  ka        correct answer for か or カ (similar for other kana).
       ([compare "help"] (displayln help)
                         (loop current letter-count kana statistics))
       ([compare "skip"] (display-many "SKIP! The correct answer was: " (romaji current))
-                        (loop (select-random-letter#io letter-count) letter-count kana statistics))
+                        (let ([new-stats (add1-to-statistics statistics (kana current) wrongs)])
+                          (loop (select-random-letter#io letter-count new-stats kana (kana current)) letter-count kana new-stats)))
       ([compare "hiragana"] (display-many "KANA-CHOICE HIRAGANA: " (map hiragana (take alphabet letter-count)))
                             (loop current letter-count hiragana statistics))
       ([compare "katakana"] (display-many "KANA-CHOICE KATAKANA: " (map katakana (take alphabet letter-count)))
                             (loop current letter-count katakana statistics))
       ([compare (romaji current)] (displayln "CORRECT!")
-                                  (~>
-                                    (add1-to-current statistics (kana current) 'right)
-                                    (loop (select-random-letter#io letter-count) letter-count kana _)))
+                                  (let ([new-stats (add1-to-statistics statistics (kana current) rights)])
+                                    (loop (select-random-letter#io letter-count new-stats kana (kana current)) letter-count kana new-stats)))
       ([is-string-digits? input] (display "LETTER-CHOICE: ")
                                  (let ([new-count (clamp-letter-count (string->number input))])
                                    (displayln (map kana (take alphabet new-count)))
                                    (loop current new-count kana statistics)))
       (else (displayln "WRONG!")
-            (loop current letter-count kana (add1-to-current statistics (kana current) 'wrong))))))
+            (loop current letter-count kana (add1-to-statistics statistics (kana current) wrongs))))))
